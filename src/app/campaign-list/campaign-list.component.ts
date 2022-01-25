4fa01d14cdc19086762041c4cdb9ba6114907bd7
Radosław Mieckowski <rmieckowski@wp.pl>
2022-01-25 15:02:35 +0100

    create mock content of the campaign-list table

--- src/app/campaign-list/campaign-list.component.ts
+++ src/app/campaign-list/campaign-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Campaign } from '../campaign';
 
 @Component({
   selector: 'app-campaign-list',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CampaignListComponent implements OnInit {
 
+  campaignes:Campaign[];
   constructor() { }
 
   ngOnInit(): void {
+    this.campaignes=[
+      {
+        id:1,
+        name:"name1",
+        keywords:'keywords',
+        bidAmount:10,
+        fund:100,
+        status:true,
+        town:'town1',
+        radius:10
+      }
+    ]
   }
 
 }
